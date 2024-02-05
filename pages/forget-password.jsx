ba9e7067cd7e18800eddfda4f0fb7a9dd1b9fbc9
Surojit <surojitdas@technoexponent.com>
2024-02-05 13:03:31 +0530

    register & login html

--- pages/forget-password.jsx
+++ pages/forget-password.jsx
@@ -15,8 +15,33 @@ const ForgetPassword = () => {
         <title>Forget Password</title>
       </Head>
 
+      <section className="before_login_sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7 before_login_box">
+              <div className="before_login_heading">
+                <h2>Forgot Your Password</h2>
+                <p>Enter email address to receive password reset link</p>
+              </div>
+              <div className="before_login_form_wrap">
+                <div className="row">
+                  <div className="col-lg-12 form-group">
+                    <label>Email or Phone number or ID</label>
+                    <div className="form_field_wrap">
+                      <input type="text" className="form-control" placeholder="Enter Your Email or Phone number or ID"/>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 form-group">
+                    <button type="button" className="form-control submit_button">Reset Password</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      Forget Password
+      
       {/* <img src="../images/favicon.ico" />
       <button onClick={()=> Router.push("/home")} className='button-sub'>Goto Home</button>
       <button onClick={()=> Router.push("/details/page")} className='button-sub'>Goto Page</button> */}
--- pages/login.jsx
+++ pages/login.jsx
@@ -15,11 +15,75 @@ const Login = () => {
         <title>Login</title>
       </Head>
 
+      <section className="before_login_sec">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-7 before_login_box">
+              <div className="before_login_heading">
+                <h2>Login</h2>
+                <p>Login To Your account</p>
+              </div>
+              <div className="before_login_form_wrap">
+                <div className="row">
+                  <div className="col-lg-12 form-group">
+                    <label>Email or Phone number or ID</label>
+                    <div className="form_field_wrap">
+                      <input type="text" className="form-control" placeholder="Enter Your Email or Phone number or ID"/>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 form-group">
+                    <label>Password</label>
+                    <div className="form_field_wrap">
+                      <input type="password" className="form-control" placeholder="Enter Your Login Password "/>
+                      <span className="show_password"><img src="images/eyeslash.svg"/></span>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 form-group">
+                    <div className="terms_check remember_forgot">
+                        <label className="remember_checkbox">
+                            <input type="checkbox" name=""/>
+                            <span className="checkmark"></span>
+                            Remember Me
+                        </label>
+                        <a href="#" onClick={()=> Router.push("/forget-password")}>Forgot Password </a>
+                    </div>
+                  </div>
+                  <div className="col-lg-12 form-group">
+                    <button type="button" className="form-control submit_button">Login</button>
+                  </div>
+                  <div className="col-lg-12 form-group text-center allready_account">
+                    <span>Don’t have an account? <a href="#" onClick={()=> Router.push("/register")}>Sign in</a></span>
+                  </div>
+                  <div className="login_or">
+                    <span>Or</span>
+                  </div>
+                  <div className="social_login">
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <img src="images/facebook-icon.png"/>
+                          <span>Sign In with Facebook</span>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <img src="images/google-icon.png"/>
+                          <span>Sign In with Google</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      Login
-      <img src="../images/favicon.ico" />
+      
+      {/* <img src="../images/favicon.ico" />
       <button onClick={()=> Router.push("/home")} className='button-sub'>Goto Home</button>
-      <button onClick={()=> Router.push("/details/page")} className='button-sub'>Goto Page</button>
+      <button onClick={()=> Router.push("/details/page")} className='button-sub'>Goto Page</button> */}
     </SiteLayout>
   )
 }
