4e2964e1bf19cc5b63b2f1610b81fa002b2a71fa
aruptechno93 <arup.dey@technoexponent.co.in>
2024-02-27 12:16:13 +0530

    add: layout, page file

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from "next";
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Welcome to Next.js",
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className}`}>{children}</body>
+    </html>
+  );
+}
