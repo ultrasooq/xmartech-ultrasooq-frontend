8c9ac7d67a4b2bf47b494809f1c02a61e4bd904a
aruptechno93 <arup.dey@technoexponent.co.in>
2024-02-28 11:14:48 +0530

    update: tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,24 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "dark-orange": "#DB2302",
+        "dark-cyan": "#022335",
+        "light-gray": "#7b7b7b",
+        "olive-green": "#669d03",
+        "color-dark": "#333",
+        "color-blue": "#1d77d1",
+        "color-yellow": "#FCB800",
+        "light-green": "#4fd992",
+      },
+      screens: {
+        xl: "1140px",
+
+        "2xl": "1140px",
+      },
+      fontFamily: {
+        body_text: ["Work Sans", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
