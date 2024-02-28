a955f573c1a52c8c06c094b0c1527dd882df0944
aruptechno93 <arup.dey@technoexponent.co.in>
2024-02-28 10:37:49 +0530

    update: add layout page for app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,37 @@
 import { Metadata } from "next";
 import "@/app/ui/global.css";
+import "../scss/before-login.scss";
+import "../scss/header.scss";
+import "../scss/home.scss";
+import "../scss/main.scss";
+import "../scss/profile-details.scss";
+import "../scss/rfq.scss";
+import "../scss/sidebar.scss";
+import "../scss/team-members.scss";
+import "../scss/trending.scss";
 import { inter } from "@/app/ui/fonts";
+import Sidebar from "@/layout/MainLayout/Sidebar";
+import Header from "@/layout/MainLayout/Header";
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "Welcome to Next.js",
+  title: "Puremoon",
+  // description: "Welcome to Next.js",
 };
 
 export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <main className="overflow-x-hidden">
+          <Sidebar />
+          <Header />
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
