2be9fd11f29fb1ad76c2cc89018251c623a4fef2
aruptechno93 <arup.dey@technoexponent.co.in>
2024-03-05 10:49:06 +0530

    update: constants file with days-of-week

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,11 @@
 export const PUREMOON_TOKEN_KEY: string = "puremoon_accessToken";
+
+export const DAYS_OF_WEEK: string[] = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+  ]
