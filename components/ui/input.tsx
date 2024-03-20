8b63b3badc9dd632d2991c07e5b1dbb1d5abc51c
aruptechno93 <arup.dey@technoexponent.co.in>
2024-03-20 18:59:46 +0530

    fix: password hide/show issue

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-
+import { EyeClosedIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
@@ -7,16 +7,38 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
     return (
-      <input
-        type={type}
-        className={cn(
-          "border-input placeholder:text-muted-foreground focus-visible:ring-ring flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
-        ref={ref}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          type={
+            type === "password" ? (showPassword ? "text" : "password") : type
+          }
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === "password" ? (
+          <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
+            {showPassword ? (
+              <EyeOpenIcon
+                className="h-5 w-5"
+                onClick={togglePasswordVisibility}
+              />
+            ) : (
+              <EyeClosedIcon
+                className="h-5 w-5"
+                onClick={togglePasswordVisibility}
+              />
+            )}
+          </div>
+        ) : null}
+      </div>
     );
   },
 );
