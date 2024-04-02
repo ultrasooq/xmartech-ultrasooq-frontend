8d18c0ffc5c98cbf431e176a3745c1eddd2674da
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-02 11:39:33 +0530

    wip: custom fields

--- components/modules/createProduct/ProductDetailsSection.tsx
+++ components/modules/createProduct/ProductDetailsSection.tsx
@@ -22,6 +22,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
 import { CalendarIcon } from "@radix-ui/react-icons";
@@ -29,30 +30,29 @@ import { format } from "date-fns";
 
 type ProductDetailsSectionProps = {};
 
+type Field = {
+  key: string;
+  type: string;
+  field: JSX.Element;
+};
+
 const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = () => {
   const formContext = useFormContext();
   const [isCustomFieldModalOpen, setIsCustomFieldModalOpen] = useState(false);
-  const [customfields, setCustomFields] = useState<
-    {
-      key: string;
-      type: string;
-      field: JSX.Element;
-    }[]
-  >([]);
+  const [customfields, setCustomFields] = useState<Field[]>([]);
+  const [customFieldType, setCustomFieldType] = useState<string>();
+
   const handleToggleCustomFieldModal = () => {
     setIsCustomFieldModalOpen(!isCustomFieldModalOpen);
   };
 
   const deleteCustomField = (key: string) => {
     setCustomFields(customfields.filter((item) => item.key !== key));
+    setCustomFieldType(undefined);
   };
 
   const handleCustomFields = (type: string) => {
-    const tempArr: {
-      key: string;
-      type: string;
-      field: JSX.Element;
-    }[] = [];
+    const tempArr: Field[] = [];
 
     switch (type) {
       case "text":
@@ -264,71 +264,254 @@ const ProductDetailsSection: React.FC<ProductDetailsSectionProps> = () => {
   };
 
   return (
-    <div className="flex w-full flex-wrap">
-      <div className="mt-2.5 w-full">
-        <label className="mb-3.5 block text-left text-lg font-medium capitalize leading-5 text-color-dark">
-          Product Details
-        </label>
-      </div>
-
-      <div className="mb-3.5 w-full">
-        <div className="flex flex-wrap">
-          {customfields.map((item) => (
-            <div key={uuidv4()} className="flex w-full items-start">
-              <button
-                type="button"
-                className="w-full flex-1 text-left"
-                onClick={() => {}}
-              >
-                {item.field}
-              </button>
-
-              <button
-                type="button"
-                onClick={() => deleteCustomField(item.key)}
-                className="flex cursor-pointer items-center bg-transparent p-0 text-sm font-semibold capitalize text-dark-orange shadow-none hover:bg-transparent"
-              >
-                <Image
-                  src="/images/social-delete-icon.svg"
-                  height={35}
-                  width={35}
-                  alt="social-delete-icon"
-                />
-              </button>
-            </div>
-          ))}
-
-          <div className="relative w-full">
-            <Button
-              variant="outline"
-              type="button"
-              onClick={handleToggleCustomFieldModal}
-              className="border-0 text-sm font-semibold capitalize text-dark-orange shadow-none hover:text-dark-orange"
-            >
-              <Image
-                src="/images/plus-orange.png"
-                className="mr-2"
-                alt="plus-orange-icon"
-                height={14}
-                width={14}
-              />
-              Add Custom Field
-            </Button>
+    <div className="grid w-full grid-cols-4 gap-x-5">
+      <div className="col-span-3 mb-3 w-full rounded-lg border border-solid border-gray-300 bg-white p-6 shadow-sm sm:p-4 lg:p-8">
+        <div className="flex w-full flex-wrap">
+          <div className="mt-2.5 w-full">
+            <label className="mb-3.5 block text-left text-lg font-medium capitalize leading-5 text-color-dark">
+              Product Details
+            </label>
           </div>
+
+          <div className="mb-3.5 w-full">
+            <div className="flex flex-wrap">
+              {customfields.map((item) => (
+                <div key={uuidv4()} className="flex w-full items-start">
+                  <button
+                    type="button"
+                    className="w-full flex-1 text-left"
+                    onClick={() => setCustomFieldType(item.type)}
+                  >
+                    {item.field}
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={() => deleteCustomField(item.key)}
+                    className="flex cursor-pointer items-center bg-transparent p-0 text-sm font-semibold capitalize text-dark-orange shadow-none hover:bg-transparent"
+                  >
+                    <Image
+                      src="/images/social-delete-icon.svg"
+                      height={35}
+                      width={35}
+                      alt="social-delete-icon"
+                    />
+                  </button>
+                </div>
+              ))}
+
+              <div className="relative w-full">
+                <Button
+                  variant="outline"
+                  type="button"
+                  onClick={handleToggleCustomFieldModal}
+                  className="border-0 text-sm font-semibold capitalize text-dark-orange shadow-none hover:text-dark-orange"
+                >
+                  <Image
+                    src="/images/plus-orange.png"
+                    className="mr-2"
+                    alt="plus-orange-icon"
+                    height={14}
+                    width={14}
+                  />
+                  Add Custom Field
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <Dialog
+            open={isCustomFieldModalOpen}
+            onOpenChange={handleToggleCustomFieldModal}
+          >
+            <DialogContent className="gap-0 p-0">
+              <CustomFieldContent
+                setFieldType={handleCustomFields}
+                onClose={handleToggleCustomFieldModal}
+              />
+            </DialogContent>
+          </Dialog>
         </div>
       </div>
 
-      <Dialog
-        open={isCustomFieldModalOpen}
-        onOpenChange={handleToggleCustomFieldModal}
-      >
-        <DialogContent className="gap-0 p-0">
-          <CustomFieldContent
-            setFieldType={handleCustomFields}
-            onClose={handleToggleCustomFieldModal}
-          />
-        </DialogContent>
-      </Dialog>
+      <div className="col-span-1 w-full">
+        {customFieldType === "text" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 flex w-full flex-col gap-y-2">
+                <Label>Size</Label>
+                <select className="!h-[48px] w-full rounded border !border-gray-300 px-3 text-sm focus-visible:!ring-0">
+                  <option value="">Select</option>
+                  <option value="full">Full</option>
+                  <option value="small">Small</option>
+                </select>
+              </div>
+
+              <div className="mb-4 flex w-full flex-col gap-y-2">
+                <Label>Input Type</Label>
+                <select className="!h-[48px] w-full rounded border !border-gray-300 px-3 text-sm focus-visible:!ring-0">
+                  <option value="">Select</option>
+                  <option value="characters">Characters</option>
+                  <option value="numbers">Numbers</option>
+                </select>
+              </div>
+
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Placeholder</Label>
+                <Input />
+              </div>
+            </CardContent>
+          </Card>
+        ) : customFieldType === "textarea" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 flex w-full flex-col gap-y-2">
+                <Label>Size</Label>
+                <select className="!h-[48px] w-full rounded border !border-gray-300 px-3 text-sm focus-visible:!ring-0">
+                  <option value="">Select</option>
+                  <option value="full">Full</option>
+                  <option value="small">Small</option>
+                </select>
+              </div>
+
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Placeholder</Label>
+                <Input />
+              </div>
+            </CardContent>
+          </Card>
+        ) : customFieldType === "dropdown" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 flex w-full flex-col gap-y-2">
+                <Label>Size</Label>
+                <select className="!h-[48px] w-full rounded border !border-gray-300 px-3 text-sm focus-visible:!ring-0">
+                  <option value="">Select</option>
+                  <option value="full">Full</option>
+                  <option value="small">Small</option>
+                </select>
+              </div>
+
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Add</Label>
+              </div>
+            </CardContent>
+          </Card>
+        ) : customFieldType === "checkbox" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Add</Label>
+              </div>
+            </CardContent>
+          </Card>
+        ) : customFieldType === "radio" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Add</Label>
+              </div>
+            </CardContent>
+          </Card>
+        ) : customFieldType === "date" ? (
+          <Card className="w-full pt-6">
+            <CardContent>
+              <div className="mb-4 space-y-2">
+                <Label className="text-sm font-normal">Label</Label>
+                <Input />
+              </div>
+
+              <div className="mb-4 mr-4 flex flex-row items-start space-x-3 space-y-0">
+                <Checkbox
+                  // checked={field.value}
+                  // onCheckedChange={field.onChange}
+                  className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                />
+                <Label className="text-sm font-normal">Required</Label>
+              </div>
+
+              <div className="mb-4 flex w-full flex-col gap-y-2">
+                <Label>Size</Label>
+                <select className="!h-[48px] w-full rounded border !border-gray-300 px-3 text-sm focus-visible:!ring-0">
+                  <option value="">Select</option>
+                  <option value="full">Full</option>
+                  <option value="small">Small</option>
+                </select>
+              </div>
+            </CardContent>
+          </Card>
+        ) : null}
+      </div>
     </div>
   );
 };
