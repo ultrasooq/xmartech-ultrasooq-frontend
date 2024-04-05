40113002e7867c1bf20eb4c31aed9afa88149d68
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-05 17:16:22 +0530

    add: react-quill editor

---
+++ components/shared/Quill/QuillEditor.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+type QuillEditorProps = {
+  onChange: (value: string) => void;
+  description: string;
+};
+
+const QuillEditor: React.FC<QuillEditorProps> = ({ onChange, description }) => {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    setValue(description || "");
+  }, [description]);
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={(e) => {
+        setValue(e);
+        onChange(e);
+      }}
+    />
+  );
+};
+
+export default QuillEditor;
