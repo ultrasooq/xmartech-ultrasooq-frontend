e60cfe7bcb1e9e68529fc67b99a81690eb8863e8
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-09 17:55:40 +0530

    add: controlled checkbox, radio, rte

---
+++ components/shared/Forms/ControlledCheckboxInput.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useFormContext } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface ControlledCheckboxInputProps {
+  label: string;
+  name: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
+}
+
+const ControlledCheckboxInput: React.FC<ControlledCheckboxInputProps> = ({
+  label,
+  name,
+  options,
+}) => {
+  const formContext = useFormContext();
+
+  return (
+    <FormField
+      control={formContext.control}
+      name={name}
+      render={() => (
+        <FormItem>
+          <div className="mb-4">
+            <FormLabel className="text-base">{label}</FormLabel>
+          </div>
+          {options.map((item) => (
+            <FormField
+              key={item.value}
+              control={formContext.control}
+              name="items"
+              render={({ field }) => {
+                return (
+                  <FormItem
+                    key={item.value}
+                    className="flex flex-row items-start space-x-3 space-y-0"
+                  >
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value?.includes(item.value)}
+                        onCheckedChange={(checked) => {
+                          return checked
+                            ? field.onChange([...field.value, item.value])
+                            : field.onChange(
+                                field.value?.filter(
+                                  (value: string) => value !== item.value,
+                                ),
+                              );
+                        }}
+                      />
+                    </FormControl>
+                    <FormLabel className="font-normal">{item.label}</FormLabel>
+                  </FormItem>
+                );
+              }}
+            />
+          ))}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default ControlledCheckboxInput;
