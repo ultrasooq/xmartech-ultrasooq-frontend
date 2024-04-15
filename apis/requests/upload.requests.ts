83443b18c6a020f4ddd1a9ea090b85efafab29f6
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-15 13:28:23 +0530

    add: multipleFile upload, delete queries, requests

--- apis/requests/upload.requests.ts
+++ apis/requests/upload.requests.ts
@@ -14,3 +14,29 @@ export const uploadFile = (payload: any) => {
     },
   });
 };
+
+export const uploadMultipleFile = (payload: any) => {
+  return axios({
+    method: "POST",
+    url: `${process.env.NEXT_PUBLIC_API_URL}/user/presignedUrlUploadMultiple`,
+    data: payload,
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+      Authorization: "Bearer " + getCookie(PUREMOON_TOKEN_KEY),
+    },
+  });
+};
+
+export const deleteFile = (payload: any) => {
+  return axios({
+    method: "DELETE",
+    url: `${process.env.NEXT_PUBLIC_API_URL}/user/presignedUrlDelete`,
+    data: payload,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + getCookie(PUREMOON_TOKEN_KEY),
+    },
+  });
+};
