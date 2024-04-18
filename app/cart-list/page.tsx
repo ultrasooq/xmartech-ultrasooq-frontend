8bce26f3f26ff83016767617ec4d70ba3eb12115
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-18 17:53:04 +0530

    wip: cart-list functionality

--- app/cart-list/page.tsx
+++ app/cart-list/page.tsx
@@ -1,255 +1,191 @@
-import React from "react";
+"use client";
+import {
+  useCartListByUserId,
+  useDeleteCartItem,
+  useUpdateCartWithLogin,
+} from "@/apis/queries/cart.queries";
+import ProductCard from "@/components/modules/cartList/ProductCard";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useToast } from "@/components/ui/use-toast";
+import Link from "next/link";
+import React, { useMemo } from "react";
 
 const CartListPage = () => {
+  const { toast } = useToast();
+
+  const cartListByUser = useCartListByUserId({
+    page: 1,
+    limit: 10,
+  });
+  const updateCartWithLogin = useUpdateCartWithLogin();
+  const deleteCartItem = useDeleteCartItem();
+
+  const memoizedCartList = useMemo(() => {
+    return cartListByUser.data?.data;
+  }, [cartListByUser.data?.data]);
+
+  const memoizedTotalAmount = useMemo(() => {
+    if (cartListByUser.data?.data?.length) {
+      return cartListByUser.data?.data?.reduce(
+        (
+          acc: number,
+          curr: {
+            productDetails: {
+              offerPrice: string;
+            };
+            quantity: number;
+          },
+        ) => {
+          return acc + +curr.productDetails.offerPrice * curr.quantity;
+        },
+        0,
+      );
+    }
+  }, [cartListByUser.data?.data?.length]);
+
+  const handleAddToCart = async (
+    quantity: number,
+    productId: number,
+    actionType: "add" | "remove",
+  ) => {
+    console.log("add to cart:", quantity, productId, actionType);
+    // return;
+    const response = await updateCartWithLogin.mutateAsync({
+      productId,
+      quantity,
+    });
+
+    if (response.status) {
+      toast({
+        title: `Item ${actionType === "add" ? "added to" : actionType === "remove" ? "removed from" : ""} cart`,
+        description: "Check your cart for more details",
+        variant: "success",
+      });
+    }
+  };
+
+  const handleRemoveItemFromCart = async (cartId: number) => {
+    console.log("cart id:", cartId);
+    // return;
+    const response = await deleteCartItem.mutateAsync({ cartId });
+    if (response.status) {
+      toast({
+        title: "Item removed from cart",
+        description: "Check your cart for more details",
+        variant: "success",
+      });
+    }
+  };
+  console.log(memoizedCartList);
+
   return (
     <div className="cart-page">
       <div className="container m-auto px-3">
-      <div className="headerPart">
-              <div className="lediv">
-                <h3>My Cart</h3>
-              </div>
-            </div>
+        <div className="headerPart">
+          <div className="lediv">
+            <h3>My Cart</h3>
+          </div>
+        </div>
         <div className="cart-page-wrapper">
-          
           <div className="cart-page-left">
-            
             <div className="bodyPart">
               <div className="card-item selected-address">
-              <div className="selected-address-lists">
+                <div className="selected-address-lists">
                   <div className="selected-address-item">
-                <div className="selectTag-lists">
-                  <div className="selectTag">Home</div>
-                </div>
-                <div className="left-address-with-right-btn">
-                  <div className="left-address">
-                    <h4>John Doe</h4>
-                    <ul>
-                      <li>
-                        <p>
-                          <span className="icon-container"><img src="/images/phoneicon.svg" alt="" /></span>
-                          <span className="text-container">+1 000 0000 0000</span>
-                        </p>
-                      </li>
-                      <li>
-                        <p>
-                          <span className="icon-container"><img src="/images/locationicon.svg" alt="" /></span>
-                          <span className="text-container">2207 Jericho Turnpike Commack North Dakota 11725</span>
-                        </p>
-                      </li>
-                    </ul>
+                    {/* <div className="selectTag-lists">
+                      <div className="selectTag">Home</div>
+                    </div> */}
+                    <div className="left-address-with-right-btn">
+                      <div className="left-address">
+                        <h4>John Doe</h4>
+                        <ul>
+                          <li>
+                            <p>
+                              <span className="icon-container">
+                                <img src="/images/phoneicon.svg" alt="" />
+                              </span>
+                              <span className="text-container">
+                                +1 000 0000 0000
+                              </span>
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <span className="icon-container">
+                                <img src="/images/locationicon.svg" alt="" />
+                              </span>
+                              <span className="text-container">
+                                2207 Jericho Turnpike Commack North Dakota 11725
+                              </span>
+                            </p>
+                          </li>
+                        </ul>
+                      </div>
+                      <div className="right-action">
+                        <a href="#" className="changebtn">
+                          Change
+                        </a>
+                      </div>
+                    </div>
                   </div>
-                  <div className="right-action">
-                    <a href="#" className="changebtn">Change</a>
-                  </div>
                 </div>
-              </div>
-              </div>
               </div>
               <div className="card-item cart-items">
-              <div className="card-inner-headerPart">
+                <div className="card-inner-headerPart">
                   <div className="lediv">
-                    <h3>cart items</h3>
+                    <h3>Cart items</h3>
                   </div>
                 </div>
                 <div className="cart-item-lists">
-                  <div className="cart-item-list-col">
-                    <figure>
-                      <div className="image-container">
-                        <img src="/images/prodvr1.png" alt="" />
+                  <div className="px-3">
+                    {cartListByUser.isLoading ? (
+                      <div className="my-3 space-y-3">
+                        {Array.from({ length: 3 }).map((_, i) => (
+                          <Skeleton key={i} className="h-28 w-full" />
+                        ))}
                       </div>
-                      <figcaption>
-                        <h4><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</a></h4>
-                        <div className="custom-form-group">
-                          <label>Quantity</label>
-                          <div className="qty-up-down-s1-with-rgMenuAction">
-                            <div className="qty-up-down-s1">
-                              <input type="number" className="custom-form-control-s1"></input>
-                              <button type="button" className="upDownBtn minus">
-                                <img src="images/upDownBtn-minus.svg" alt="" />
-                              </button>
-                              <button type="button" className="upDownBtn plus">
-                                <img src="images/upDownBtn-plus.svg" alt="" />
-                              </button>
-                            </div>
-                            <ul className="rgMenuAction-lists">
-                              <li>
-                                <a>Remove</a>
-                              </li>
-                              <li>
-                                <a>Move to wishlist</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                    <div className="right-info">
-                      <h6>Price</h6>
-                      <h5>$332.38</h5>
-                    </div>
+                    ) : null}
                   </div>
-                  <div className="cart-item-list-col">
-                    <figure>
-                      <div className="image-container">
-                        <img src="/images/prodvr1.png" alt="" />
-                      </div>
-                      <figcaption>
-                        <h4><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</a></h4>
-                        <div className="custom-form-group">
-                          <label>Quantity</label>
-                          <div className="qty-up-down-s1-with-rgMenuAction">
-                            <div className="qty-up-down-s1">
-                              <input type="number" className="custom-form-control-s1"></input>
-                              <button type="button" className="upDownBtn minus">
-                                <img src="images/upDownBtn-minus.svg" alt="" />
-                              </button>
-                              <button type="button" className="upDownBtn plus">
-                                <img src="images/upDownBtn-plus.svg" alt="" />
-                              </button>
-                            </div>
-                            <ul className="rgMenuAction-lists">
-                              <li>
-                                <a>Remove</a>
-                              </li>
-                              <li>
-                                <a>Move to wishlist</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                    <div className="right-info">
-                      <h6>Price</h6>
-                      <h5>$332.38</h5>
-                    </div>
-                  </div>
-                  <div className="cart-item-list-col">
-                    <figure>
-                      <div className="image-container">
-                        <img src="/images/prodvr1.png" alt="" />
-                      </div>
-                      <figcaption>
-                        <h4><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</a></h4>
-                        <div className="custom-form-group">
-                          <label>Quantity</label>
-                          <div className="qty-up-down-s1-with-rgMenuAction">
-                            <div className="qty-up-down-s1">
-                              <input type="number" className="custom-form-control-s1"></input>
-                              <button type="button" className="upDownBtn minus">
-                                <img src="images/upDownBtn-minus.svg" alt="" />
-                              </button>
-                              <button type="button" className="upDownBtn plus">
-                                <img src="images/upDownBtn-plus.svg" alt="" />
-                              </button>
-                            </div>
-                            <ul className="rgMenuAction-lists">
-                              <li>
-                                <a>Remove</a>
-                              </li>
-                              <li>
-                                <a>Move to wishlist</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                    <div className="right-info">
-                      <h6>Price</h6>
-                      <h5>$332.38</h5>
-                    </div>
-                  </div>
-                  <div className="cart-item-list-col">
-                    <figure>
-                      <div className="image-container">
-                        <img src="/images/prodvr1.png" alt="" />
-                      </div>
-                      <figcaption>
-                        <h4><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</a></h4>
-                        <div className="custom-form-group">
-                          <label>Quantity</label>
-                          <div className="qty-up-down-s1-with-rgMenuAction">
-                            <div className="qty-up-down-s1">
-                              <input type="number" className="custom-form-control-s1"></input>
-                              <button type="button" className="upDownBtn minus">
-                                <img src="images/upDownBtn-minus.svg" alt="" />
-                              </button>
-                              <button type="button" className="upDownBtn plus">
-                                <img src="images/upDownBtn-plus.svg" alt="" />
-                              </button>
-                            </div>
-                            <ul className="rgMenuAction-lists">
-                              <li>
-                                <a>Remove</a>
-                              </li>
-                              <li>
-                                <a>Move to wishlist</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                    <div className="right-info">
-                      <h6>Price</h6>
-                      <h5>$332.38</h5>
-                    </div>
-                  </div>
-                  <div className="cart-item-list-col">
-                    <figure>
-                      <div className="image-container">
-                        <img src="/images/prodvr1.png" alt="" />
-                      </div>
-                      <figcaption>
-                        <h4><a href="">Lorem ipsum dolor sit amet, consectetur adipiscing elit,</a></h4>
-                        <div className="custom-form-group">
-                          <label>Quantity</label>
-                          <div className="qty-up-down-s1-with-rgMenuAction">
-                            <div className="qty-up-down-s1">
-                              <input type="number" className="custom-form-control-s1"></input>
-                              <button type="button" className="upDownBtn minus">
-                                <img src="images/upDownBtn-minus.svg" alt="" />
-                              </button>
-                              <button type="button" className="upDownBtn plus">
-                                <img src="images/upDownBtn-plus.svg" alt="" />
-                              </button>
-                            </div>
-                            <ul className="rgMenuAction-lists">
-                              <li>
-                                <a>Remove</a>
-                              </li>
-                              <li>
-                                <a>Move to wishlist</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </figcaption>
-                    </figure>
-                    <div className="right-info">
-                      <h6>Price</h6>
-                      <h5>$332.38</h5>
-                    </div>
-                  </div>
+                  {memoizedCartList?.map(
+                    (item: {
+                      id: number;
+                      productId: number;
+                      productDetails: {
+                        productName: string;
+                        offerPrice: string;
+                        productImages: { id: number; image: string }[];
+                      };
+                      quantity: number;
+                    }) => (
+                      <ProductCard
+                        key={item.id}
+                        cartId={item.id}
+                        productId={item.productId}
+                        productName={item.productDetails.productName}
+                        offerPrice={item.productDetails.offerPrice}
+                        productQuantity={item.quantity}
+                        productImages={item.productDetails.productImages}
+                        onAdd={handleAddToCart}
+                        onRemove={handleRemoveItemFromCart}
+                      />
+                    ),
+                  )}
                 </div>
               </div>
-
             </div>
           </div>
           <div className="cart-page-right">
             <div className="card-item priceDetails">
-              
               <div className="card-inner-headerPart">
-                  <div className="lediv">
-                    <h3>Price Details</h3>
-                  </div>
+                <div className="lediv">
+                  <h3>Price Details</h3>
                 </div>
+              </div>
               <div className="priceDetails-body">
                 <ul>
                   <li>
                     <p>Subtotal</p>
-                    <h5>$3332.38</h5>
+                    <h5>${memoizedTotalAmount}</h5>
                   </li>
                   <li>
                     <p>Shipping</p>
@@ -262,11 +198,13 @@ const CartListPage = () => {
               </div>
               <div className="priceDetails-footer">
                 <h4>Total Amount</h4>
-                <h4 className="amount-value">$3332.38</h4>
+                <h4 className="amount-value">${memoizedTotalAmount}</h4>
               </div>
             </div>
             <div className="order-action-btn">
-              <a href="" className="theme-primary-btn order-btn">Place Order</a>
+              <Link href="/checkout" className="theme-primary-btn order-btn">
+                Place Order
+              </Link>
             </div>
           </div>
         </div>
