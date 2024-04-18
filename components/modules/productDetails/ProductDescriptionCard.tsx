2f166766fab25250f6b6a695827ced93b3fca714
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-18 17:52:31 +0530

    add: image card, details card in product details page

--- components/modules/productDetails/ProductDescriptionCard.tsx
+++ components/modules/productDetails/ProductDescriptionCard.tsx
@@ -1,26 +1,50 @@
-import React from "react";
-import { Slider } from "@/components/ui/slider";
-import { Button } from "@/components/ui/button";
+import React, { useEffect, useMemo, useState } from "react";
 
 type ProductDescriptionCardProps = {
-  productDetails: any;
+  productName: string;
+  brand: string;
+  productPrice: string;
+  offerPrice: string;
+  skuNo: string;
+  productTags: any[];
+  category: string;
+  productQuantity: number;
+  onAdd: (args0: number) => void;
 };
 
 const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
-  productDetails,
+  productName,
+  brand,
+  productPrice,
+  offerPrice,
+  skuNo,
+  productTags,
+  category,
+  productQuantity,
+  onAdd,
 }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  useEffect(() => {
+    setQuantity(productQuantity);
+  }, [productQuantity]);
+
   return (
     <div className="product-view-s1-right">
       <div className="info-col">
-        <h2>{productDetails?.productName}</h2>
+        <h2>{productName}</h2>
       </div>
       <div className="info-col mb-2">
         <div className="brand_sold_info">
           <div className="lediv">
-            <h5><span>Brand:</span> new</h5>
+            <h5>
+              <span>Brand:</span> {brand}
+            </h5>
           </div>
           <div className="rgdiv">
-            <h5><span>Sold By:</span> YOUNG SHOP</h5>
+            <h5>
+              <span>Sold By:</span> YOUNG SHOP
+            </h5>
           </div>
         </div>
         <div className="rating">
@@ -28,14 +52,11 @@ const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
           <span>(5 Reviews)</span>
         </div>
         <h3>
-          ${productDetails?.offerPrice}{" "}
-          <span>${productDetails?.productPrice}</span>
+          ${offerPrice} <span>${productPrice}</span>
         </h3>
-
       </div>
       <div className="info-col">
         <div className="row">
-
           <div className="col-12 col-md-12">
             <div className="col-12 col-md-12">
               <div className="form-group">
@@ -43,7 +64,9 @@ const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
                   <li>The standard chunk of Lorem Ipsum</li>
                   <li>The standard chunk of Lorem Ipsum</li>
                   <li>It is a long established fact that a reader</li>
-                  <li>Double-ended Coil Cord with 3.5mm Stereo Plugs Included</li>
+                  <li>
+                    Double-ended Coil Cord with 3.5mm Stereo Plugs Included
+                  </li>
                   <li>It is a long established fact that a reader</li>
                 </ul>
               </div>
@@ -56,12 +79,31 @@ const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
           <div className="quantity-with-right-payment-info">
             <div className="left-qty">
               <label>Quantity</label>
-              <div className="qty-up-down-s1">
-                <input type="number" className="custom-form-control-s1"></input>
-                <button type="button" className="upDownBtn minus">
+              <div className="flex w-28 items-center justify-center gap-x-8">
+                {/* <div className="qty-up-down-s1"> */}
+                {/* <input type="number" className="custom-form-control-s1"></input> */}
+                <button
+                  type="button"
+                  className="upDownBtn minus"
+                  onClick={() => {
+                    if (quantity > 1) {
+                      setQuantity(quantity - 1);
+                      onAdd(quantity - 1);
+                    }
+                  }}
+                  disabled={quantity === 1}
+                >
                   <img src="images/upDownBtn-minus.svg" alt="" />
                 </button>
-                <button type="button" className="upDownBtn plus">
+                <p>{quantity}</p>
+                <button
+                  type="button"
+                  className="upDownBtn plus"
+                  onClick={() => {
+                    setQuantity(quantity + 1);
+                    onAdd(quantity + 1);
+                  }}
+                >
                   <img src="images/upDownBtn-plus.svg" alt="" />
                 </button>
               </div>
@@ -69,11 +111,11 @@ const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
             <div className="right-payment-info">
               <ul>
                 <li>
-                <img src="/images/securePaymenticon.svg" alt=""/>
+                  <img src="/images/securePaymenticon.svg" alt="" />
                   <span>Secure Payment</span>
                 </li>
                 <li>
-                  <img src="/images/support-24hr.svg" alt=""/>
+                  <img src="/images/support-24hr.svg" alt="" />
                   <span>Secure Payment</span>
                 </li>
               </ul>
@@ -87,13 +129,16 @@ const ProductDescriptionCard: React.FC<ProductDescriptionCardProps> = ({
             <div className="form-group mb-0">
               <label>Report Abuse</label>
               <p>
-                <span className="color-text">SKU:</span> SF1133569600-1
+                <span className="color-text">SKU:</span> {skuNo}
               </p>
               <p>
-                <span className="color-text">Categories:</span> Consumer Electronics, Refrigerator Babies & Moms
+                <span className="color-text">Categories:</span> {category}
               </p>
               <p>
-                <span className="color-text">Tags:</span> sofa, technologies, wireless
+                <span className="color-text">Tags:</span>{" "}
+                {productTags
+                  ?.map((item) => item.productTagsTag?.tagName)
+                  .join(", ")}
               </p>
             </div>
           </div>
--- components/modules/productDetails/ProductImagesCard.tsx
+++ components/modules/productDetails/ProductImagesCard.tsx
@@ -1,19 +1,27 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import validator from "validator";
 
 type ProductImagesCardProps = {
   productDetails: any;
+  onAdd: () => void;
+  onNavigate: () => void;
+  hasItem: boolean;
 };
 
 const ProductImagesCard: React.FC<ProductImagesCardProps> = ({
   productDetails,
+  onAdd,
+  onNavigate,
+  hasItem,
 }) => {
   return (
     <div className="product-view-s1-left">
-      <div className="product-view-s1">
-        <div className="product-view-s1-big-image">
-            <img
+      <div className="grid grid-cols-4 gap-4">
+        <div className="order-2 col-span-3 max-h-[500px] space-y-4 bg-gray-100">
+          <div className="relative h-full w-full">
+            <Image
               src={
                 productDetails?.productImages?.[0]?.image &&
                 validator.isURL(productDetails.productImages[0].image)
@@ -21,32 +29,51 @@ const ProductImagesCard: React.FC<ProductImagesCardProps> = ({
                   : "/images/product-placeholder.png"
               }
               alt="primary-image"
+              fill
+              className="object-contain"
             />
+          </div>
+
+          <div className="flex w-full gap-x-3">
+            <Button
+              onClick={hasItem ? onNavigate : onAdd}
+              className="h-14 flex-1 rounded-none bg-color-yellow text-base"
+            >
+              {hasItem ? "Go To Cart" : "Add To Cart"}
+            </Button>
+            <Button className="h-14 flex-1 rounded-none bg-dark-orange text-base">
+              Buy Now
+            </Button>
+          </div>
         </div>
 
-        <div className="product-view-s1-thumb-lists">
+        <div className="col-span-1 m-auto flex flex-col gap-4">
           {productDetails?.productImages?.map((item: any, index: number) =>
             index !== 0 ? (
-              <div className="thumb-item" key={item?.id}>
-                <div className="image-container">
-                <img
+              <div className="relative h-28 w-28 bg-gray-100" key={item?.id}>
+                <Image
                   src={
                     item?.image && validator.isURL(item.image)
                       ? item.image
                       : "/images/product-placeholder.png"
                   }
                   alt="primary-image"
+                  fill
+                  className="object-contain"
                 />
-              </div>
               </div>
             ) : null,
           )}
         </div>
       </div>
-      <div className="cart-actions">
-        <button type="button" className="custom-btn cart-btn">Add To Cart</button>
-        <button type="button" className="custom-btn theme-primary-btn">Buy Now</button>
-      </div>
+      {/* <div className="cart-actions">
+        <button type="button" className="custom-btn cart-btn">
+          Add To Cart
+        </button>
+        <button type="button" className="custom-btn theme-primary-btn">
+          Buy Now
+        </button>
+      </div> */}
     </div>
   );
 };
