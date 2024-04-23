abc2c960ef126a9999df9bf504d1845d0961f550
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-23 12:15:02 +0530

    fix: image toggle

--- components/modules/productDetails/ProductImagesCard.tsx
+++ components/modules/productDetails/ProductImagesCard.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import validator from "validator";
 
 type ProductImagesCardProps = {
@@ -21,25 +22,43 @@ const ProductImagesCard: React.FC<ProductImagesCardProps> = ({
   hasItem,
   isLoading,
 }) => {
+  const [previewImages, setPreviewImages] = React.useState<any>([]);
+
+  useEffect(() => {
+    if (!productDetails?.productImages?.length) return;
+
+    setPreviewImages(
+      productDetails?.productImages?.[0]?.image &&
+        validator.isURL(productDetails.productImages[0].image)
+        ? productDetails?.productImages?.[0]?.image
+        : "/images/product-placeholder.png",
+    );
+  }, [productDetails?.productImages?.length]);
+
   return (
     <div className="product-view-s1-left">
       <div className="grid grid-cols-4 gap-4">
         <div className="order-2 col-span-3 max-h-[500px] space-y-4 bg-gray-100">
-          {productDetails?.productImages?.[0]?.image ? (
+          {productDetails?.productImages?.length ? (
             <div className="relative min-h-[500px] w-full">
               <Image
-                src={
-                  productDetails?.productImages?.[0]?.image &&
-                  validator.isURL(productDetails.productImages[0].image)
-                    ? productDetails?.productImages?.[0]?.image
-                    : "/images/product-placeholder.png"
-                }
+                src={previewImages}
+                alt="primary-image"
+                fill
+                className="object-contain"
+              />
+            </div>
+          ) : !isLoading ? (
+            <div className="relative min-h-[500px] w-full">
+              <Image
+                src="/images/product-placeholder.png"
                 alt="primary-image"
                 fill
                 className="object-contain"
               />
             </div>
           ) : null}
+
           {isLoading ? <Skeleton className="min-h-[500px] w-full" /> : null}
 
           <div className="flex w-full gap-x-3 self-end">
@@ -65,22 +84,28 @@ const ProductImagesCard: React.FC<ProductImagesCardProps> = ({
               ))
             : null}
 
-          {productDetails?.productImages?.map((item: any, index: number) =>
-            index !== 0 ? (
-              <div className="relative h-28 w-28 bg-gray-100" key={item?.id}>
-                <Image
-                  src={
-                    item?.image && validator.isURL(item.image)
-                      ? item.image
-                      : "/images/product-placeholder.png"
-                  }
-                  alt="primary-image"
-                  fill
-                  className="object-contain"
-                />
-              </div>
-            ) : null,
-          )}
+          {productDetails?.productImages?.map((item: any) => (
+            <Button
+              variant="ghost"
+              className={cn(
+                item?.image === previewImages ? "border-2 border-red-500" : "",
+                "relative h-28 w-28 rounded-none bg-gray-100",
+              )}
+              key={item?.id}
+              onClick={() => setPreviewImages(item?.image)}
+            >
+              <Image
+                src={
+                  item?.image && validator.isURL(item.image)
+                    ? item.image
+                    : "/images/product-placeholder.png"
+                }
+                alt="primary-image"
+                fill
+                className="rounded-none object-contain"
+              />
+            </Button>
+          ))}
         </div>
       </div>
     </div>
