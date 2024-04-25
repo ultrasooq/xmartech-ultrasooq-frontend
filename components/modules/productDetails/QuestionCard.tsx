f8d6b53f9954af1e6a09d80203141f726221fcd3
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-25 18:56:45 +0530

    add: question card

--- components/modules/productDetails/QuestionCard.tsx
+++ components/modules/productDetails/QuestionCard.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import AnswerForm from "./AnswerForm";
+import { cn } from "@/lib/utils";
 
 type QuestionCardProps = {
-  id?: string;
+  id: number;
   question: string;
-  answer: string;
-  userName: string;
+  answer?: string;
+  userName?: string;
+  isLastItem: boolean;
+  hasAccessToken?: boolean;
 };
 
 const QuestionCard: React.FC<QuestionCardProps> = ({
@@ -12,20 +18,49 @@ const QuestionCard: React.FC<QuestionCardProps> = ({
   question,
   answer,
   userName,
+  isLastItem,
+  hasAccessToken,
 }) => {
+  const [isQuestionModalOpen, setIsQuestionModalOpen] = useState(false);
+
+  const handleToggleQuestionModal = () =>
+    setIsQuestionModalOpen(!isQuestionModalOpen);
+
   return (
-    <div className="w-full border-b border-solid border-gray-300 p-3">
+    <div
+      className={cn(
+        !isLastItem ? "border-b border-solid border-gray-300" : "",
+        "w-full p-3",
+      )}
+    >
       <article className="space-y-1">
-        <h3>
-          <span className="mr-2 font-bold">Q:</span>
+        <h3 className="font-bold">
+          <span className="mr-2">Q:</span>
           {question}
         </h3>
         <p>
           <span className="mr-2 font-bold">A:</span>
-          {answer}
+          {answer || "No answer yet"}
         </p>
-        <p className="text-xs font-medium text-gray-500">{userName}</p>
+        <p className="text-xs font-medium text-gray-500">{userName || ""}</p>
+
+        {hasAccessToken && !answer ? (
+          <div className="!my-2 text-center">
+            <Button variant="secondary" onClick={handleToggleQuestionModal}>
+              Reply
+            </Button>
+          </div>
+        ) : null}
       </article>
+
+      <Dialog
+        open={isQuestionModalOpen}
+        onOpenChange={handleToggleQuestionModal}
+      >
+        <DialogContent>
+          <AnswerForm onClose={handleToggleQuestionModal} questionId={id} />
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
