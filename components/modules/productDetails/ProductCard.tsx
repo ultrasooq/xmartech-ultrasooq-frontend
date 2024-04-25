e16418c13b5b094c8bfd4fbf00dc4bef5a0eced8
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-25 13:51:22 +0530

    update: related product list func

--- components/modules/productDetails/ProductCard.tsx
+++ components/modules/productDetails/ProductCard.tsx
@@ -1,29 +1,63 @@
-import React from "react";
+import Image from "next/image";
+import React, { useMemo } from "react";
 
-const ProductCard = () => {
+type ProductCardProps = {
+  id: number;
+  productName: string;
+  productImages: { id: number; image: string }[];
+  shortDescription: string;
+  offerPrice: number;
+  productPrice: number;
+  onView: () => void;
+};
+
+const ProductCard: React.FC<ProductCardProps> = ({
+  id,
+  productName,
+  productImages,
+  shortDescription,
+  offerPrice,
+  productPrice,
+  onView,
+}) => {
+  const offerPercentage = useMemo(
+    () => Math.floor(100 - (Number(offerPrice) / Number(productPrice)) * 100),
+    [offerPrice, productPrice],
+  );
+
   return (
     <div className="relative border border-solid border-transparent px-2 py-1 pt-7 hover:border-gray-300">
-      <div className="absolute right-2.5 top-2.5 inline-block rounded bg-dark-orange px-2.5 py-2 text-lg font-medium capitalize leading-5 text-white">
-        <span>-6%</span>
+      <div className="absolute right-2.5 top-2.5 z-10 inline-block rounded bg-dark-orange px-2.5 py-2 text-lg font-medium capitalize leading-5 text-white">
+        <span>{!isNaN(offerPercentage) ? offerPercentage : 0}%</span>
       </div>
-      <div className="flex h-40 w-full items-center justify-center lg:h-52">
-        <img src="/images/pro-mobile4.png" />
+      <div className="relative mx-auto mb-4 h-36 w-36">
+        <Image
+          src={productImages?.[0]?.image || "/images/product-placeholder.png"}
+          alt="preview"
+          fill
+        />
       </div>
       <div className="relative w-full text-sm font-normal capitalize text-color-blue lg:text-base">
-        <h6 className="mb-2.5 border-b border-solid border-gray-300 pb-2.5 text-xs font-normal uppercase text-color-dark">
-          young shop
-        </h6>
+        <h4 className="mb-2.5 border-b border-solid border-gray-300 pb-2.5 text-xs font-normal uppercase text-color-dark">
+          {productName}
+        </h4>
         <div className="mt-2.5 w-full">
           <h4 className="font-lg font-normal uppercase text-olive-green">
-            $55.99
+            ${offerPrice}
           </h4>
         </div>
-        <p>
-          <a href="#">Lorem Ipsum is simply dummy text..</a>
+        <p className="truncate" title={shortDescription}>
+          {shortDescription}
         </p>
-        <img src="images/star.png" className="mt-3" />
+        <Image
+          src="/images/rating_stars.svg"
+          alt="star-icon"
+          width={85}
+          height={15}
+          className="mt-3"
+        />
         <span className="w-auto text-base font-normal text-light-gray">
-          $332.38
+          ${productPrice}
         </span>
       </div>
     </div>
--- components/modules/productDetails/RelatedProductsSection.tsx
+++ components/modules/productDetails/RelatedProductsSection.tsx
@@ -7,8 +7,17 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { stripHTML } from "@/utils/helper";
 
-const RelatedProductsSection = () => {
+type RelatedProductsSectionProps = {
+  relatedProducts: any[];
+  isLoading: boolean;
+};
+
+const RelatedProductsSection: React.FC<RelatedProductsSectionProps> = ({
+  relatedProducts,
+  isLoading,
+}) => {
   return (
     <section className="w-full py-8">
       <div className="container m-auto">
@@ -17,30 +26,43 @@ const RelatedProductsSection = () => {
             <h3>Related products</h3>
           </div>
         </div>
-        {/* <div className="grid w-full grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"> */}
-        <Carousel
-          className="w-full"
-          opts={{
-            align: "start",
-            loop: true,
-          }}
-        >
-          <CarouselContent className="-ml-1">
-            {Array.from({ length: 10 }).map((_, index) => (
-              <CarouselItem
-                key={index}
-                className="max-w-[260px] pl-1 md:basis-1/2 lg:basis-1/3"
-              >
-                <div className="p-1">
-                  <ProductCard />
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-        {/* </div> */}
+
+        {!isLoading && relatedProducts?.length ? (
+          <Carousel
+            className="w-full"
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+          >
+            <CarouselContent className="-ml-1">
+              {relatedProducts?.map((item: any) => (
+                <CarouselItem
+                  key={item?.id}
+                  className="max-w-[260px] pl-1 md:basis-1/2 lg:basis-1/3"
+                >
+                  <div className="p-1">
+                    <ProductCard
+                      id={item?.id}
+                      productName={item?.productName}
+                      productImages={item?.productImages}
+                      shortDescription={
+                        item?.shortDescription
+                          ? stripHTML(item?.shortDescription)
+                          : "-"
+                      }
+                      offerPrice={item?.offerPrice}
+                      productPrice={item?.productPrice}
+                      onView={() => {}}
+                    />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        ) : null}
       </div>
     </section>
   );
