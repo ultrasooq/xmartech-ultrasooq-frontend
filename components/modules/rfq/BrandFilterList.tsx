09cf4366be6ba7c6be306d0dc86ed1b605285ffa
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-30 14:20:13 +0530

    add: category, brand list and hide

---
+++ components/modules/rfq/BrandFilterList.tsx
@@ -0,0 +1,108 @@
+import React, { useState, useMemo } from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Input } from "@/components/ui/input";
+import { Checkbox } from "@/components/ui/checkbox";
+import { IBrands, ISelectOptions } from "@/utils/types/common.types";
+import { useBrands } from "@/apis/queries/masters.queries";
+import { debounce } from "lodash";
+
+const BrandFilterList = () => {
+  const [selectedBrandIds, setSelectedBrandIds] = useState<number[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const brandsQuery = useBrands({
+    term: searchTerm,
+  });
+
+  const handleDebounce = debounce((event: any) => {
+    setSearchTerm(event.target.value);
+  }, 1000);
+
+  const memoizedBrands = useMemo(() => {
+    return (
+      brandsQuery?.data?.data.map((item: IBrands) => {
+        return { label: item.brandName, value: item.id };
+      }) || []
+    );
+  }, [brandsQuery?.data?.data?.length]);
+
+  const handleBrandChange = (
+    checked: boolean | string,
+    item: ISelectOptions,
+  ) => {
+    let tempArr = selectedBrandIds || [];
+    if (checked && !tempArr.find((ele: number) => ele === item.value)) {
+      tempArr = [...tempArr, item.value];
+    }
+
+    if (!checked && tempArr.find((ele: number) => ele === item.value)) {
+      tempArr = tempArr.filter((ele: number) => ele !== item.value);
+    }
+    setSelectedBrandIds(tempArr);
+  };
+
+  return (
+    <div className="trending-search-sec">
+      <div className="container m-auto">
+        <div className="left-filter">
+          <Accordion
+            type="multiple"
+            defaultValue={["brand"]}
+            className="filter-col"
+          >
+            <AccordionItem value="brand">
+              <AccordionTrigger className="px-3 text-base hover:!no-underline">
+                By Brand
+              </AccordionTrigger>
+              <AccordionContent>
+                <div className="filter-sub-header">
+                  <Input
+                    type="text"
+                    placeholder="Search Brand"
+                    className="custom-form-control-s1 searchInput rounded-none"
+                    onChange={handleDebounce}
+                  />
+                </div>
+                <div className="filter-body-part">
+                  <div className="filter-checklists">
+                    {!memoizedBrands.length ? (
+                      <p className="text-center text-sm font-medium">
+                        No data found
+                      </p>
+                    ) : null}
+                    {memoizedBrands.map((item: ISelectOptions) => (
+                      <div key={item.value} className="div-li">
+                        <Checkbox
+                          id={item.label}
+                          className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
+                          onCheckedChange={(checked) =>
+                            handleBrandChange(checked, item)
+                          }
+                          checked={selectedBrandIds.includes(item.value)}
+                        />
+                        <div className="grid gap-1.5 leading-none">
+                          <label
+                            htmlFor={item.label}
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          >
+                            {item.label}
+                          </label>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandFilterList;
---
+++ components/modules/rfq/CategoryFilterList.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const CategoryFilterList = () => {
+  return (
+    <div className="product_filter_box">
+      <div className="product_filter_box_head">
+        <h4>Categories</h4>
+        <img src="images/symbol.svg" alt="symbol-icon" />
+      </div>
+      <div className="check_filter">
+        <div className="categori_list">
+          <a href="">Clothing & Apparel</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Garden & Kitchen</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Consumer Electrics</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Health & Beauty</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Computers & Technologies</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Jewelry & Watches</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Phones & Accessories</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Sport & Outdoor</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Babies and Moms</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Books & Office</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Cars & Motocycles</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Fruits</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Meat</a>
+        </div>
+        <div className="categori_list">
+          <a href="">Book</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryFilterList;
