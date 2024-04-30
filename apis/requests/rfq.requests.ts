18d9ca97bc3390f00e55bdefd39999ac2abb0902
aruptechno93 <arup.dey@technoexponent.co.in>
2024-04-30 14:19:48 +0530

    add: rfq queries and requests

--- apis/requests/rfq.requests.ts
+++ apis/requests/rfq.requests.ts
@@ -3,7 +3,12 @@ import { getCookie } from "cookies-next";
 import axios from "axios";
 import urlcat from "urlcat";
 
-export const fetchRfqProducts = (payload: { page: number; limit: number }) => {
+export const fetchRfqProducts = (payload: {
+  page: number;
+  limit: number;
+  term?: string;
+  adminId?: string;
+}) => {
   return axios({
     method: "GET",
     url: urlcat(
@@ -22,6 +27,52 @@ export const addRfqProduct = (payload: {
     method: "POST",
     url: `${process.env.NEXT_PUBLIC_API_URL}/product/addRfqProduct`,
     data: payload,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + getCookie(PUREMOON_TOKEN_KEY),
+    },
+  });
+};
+
+export const fetchRfqCartByUserId = (payload: {
+  page: number;
+  limit: number;
+}) => {
+  return axios({
+    method: "GET",
+    url: urlcat(`${process.env.NEXT_PUBLIC_API_URL}/cart/rfqCartlist`, payload),
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + getCookie(PUREMOON_TOKEN_KEY),
+    },
+  });
+};
+
+export const updateRfqCartWithLogin = (payload: {
+  rfqProductId: number;
+  quantity: number;
+}) => {
+  return axios({
+    method: "PATCH",
+    url: `${process.env.NEXT_PUBLIC_API_URL}/cart/updateRfqCart`,
+    data: payload,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: "Bearer " + getCookie(PUREMOON_TOKEN_KEY),
+    },
+  });
+};
+
+export const deleteRfqCartItem = (payload: { rfqCartId: number }) => {
+  return axios({
+    method: "DELETE",
+    url: urlcat(
+      `${process.env.NEXT_PUBLIC_API_URL}/cart/rfqCartDelete`,
+      payload,
+    ),
     headers: {
       "Content-Type": "application/json",
       Accept: "application/json",
