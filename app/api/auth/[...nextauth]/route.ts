7312bde5586864f0a644338394f8d1c4c70788d5
aruptechno93 <arup.dey@technoexponent.co.in>
2024-05-09 19:03:53 +0530

    update: add secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -13,6 +13,7 @@ const handler = NextAuth({
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
 export { handler as GET, handler as POST };
