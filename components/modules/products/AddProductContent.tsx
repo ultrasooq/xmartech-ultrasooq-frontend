c57e071b4ad236b86ce81d0eba8d5b269caec809
aruptechno93 <arup.dey@technoexponent.co.in>
2024-05-27 16:41:34 +0530

    update: replace with link component for add product modal

--- components/modules/products/AddProductContent.tsx
+++ components/modules/products/AddProductContent.tsx
@@ -1,54 +1,56 @@
 import React from "react";
-import {
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogContent,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
+import { DialogContent } from "@/components/ui/dialog";
 import Image from "next/image";
+import Link from "next/link";
+import AddProductIcon from "@/public/images/add-product.svg";
+import ExistingProductIcon from "@/public/images/existing-product.svg";
 
-type AddProductContentProps = {
-  onToExistingProduct: () => void;
-  onToNewProduct: () => void;
-};
+type AddProductContentProps = {};
 
-const AddProductContent: React.FC<AddProductContentProps> = ({
-  onToExistingProduct,
-  onToNewProduct,
-}) => {
+const AddProductContent: React.FC<AddProductContentProps> = () => {
   return (
     <DialogContent className="custom-action-type-chose-picker">
       <div className="modal-headerpart">
         <h5>Chose Add Product Type</h5>
-        {/* <button type="button" className="closebtn"><img src="/images/close.svg" alt=""/></button> */}
       </div>
       <div className="modal-bodypart">
         <div className="import-pickup-type-selector-lists">
           <div className="import-pickup-type-selector-item">
-            <input type="radio" className="select-controller" name="isStartNewImport" onClick={onToNewProduct} />
-            <div className="import-pickup-type-selector-box">
+            <Link
+              href="/product"
+              className="import-pickup-type-selector-box hover:!bg-gray-100"
+            >
               <div className="icon-container">
-                <img src="/images/add-product.svg" alt="" />
+                <Image src={AddProductIcon} alt="add-product-icon" />
               </div>
               <div className="text-container">
                 <h5>Add a new product</h5>
-                <p>Lorem Ipsum is simply Lorem 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>
+                  Lorem Ipsum is simply Lorem 1500s, when an unknown printer
+                  took a galley of type and scrambled it to make a type specimen
+                  book.
+                </p>
               </div>
-            </div>
+            </Link>
           </div>
+
           <div className="import-pickup-type-selector-item">
-            <input type="radio" onClick={onToExistingProduct} className="select-controller" name="isStartNewImport" />
-            <div className="import-pickup-type-selector-box">
+            <Link
+              href="/rfq-list"
+              className="import-pickup-type-selector-box hover:!bg-gray-100"
+            >
               <div className="icon-container">
-                <img src="/images/existing-product.svg" alt="" />
+                <Image src={ExistingProductIcon} alt="add-product-icon" />
               </div>
               <div className="text-container">
                 <h5>Choose from Existing product </h5>
-                <p>Lorem Ipsum is simply Lorem  1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>
+                  Lorem Ipsum is simply Lorem 1500s, when an unknown printer
+                  took a galley of type and scrambled it to make a type specimen
+                  book.
+                </p>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
