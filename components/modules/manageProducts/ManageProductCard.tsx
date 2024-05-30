d3cefd8a418f13c6e3d0347cc9c110f8566726ce
aruptechno93 <arup.dey@technoexponent.co.in>
2024-05-30 16:39:39 +0530

    add: manage product card

--- components/modules/manageProducts/ManageProductCard.tsx
+++ components/modules/manageProducts/ManageProductCard.tsx
@@ -1,17 +1,54 @@
 import React from "react";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import PlaceholderImage from "@/public/images/product-placeholder.png";
+import Image from "next/image";
+import validator from "validator";
 
 type ManageProductCardProps = {
   selectedIds?: number[];
   onSelectedId?: (args0: boolean | string, args1: number) => void;
   id: number;
+  productImage: string | null;
+  productName: string;
+  productPrice: string;
+  deliveryAfter: number;
+  productLocation: string;
+  consumerType: string;
+  sellType: string;
+  timeOpen: number | null;
+  timeClose: number | null;
+  vendorDiscount: number | null;
+  consumerDiscount: number | null;
+  minQuantity: number | null;
+  maxQuantity: number | null;
+  minCustomer: number | null;
+  maxCustomer: number | null;
+  minQuantityPerCustomer: number | null;
+  maxQuantityPerCustomer: number | null;
 };
 
 const ManageProductCard: React.FC<ManageProductCardProps> = ({
   selectedIds,
   onSelectedId,
   id,
+  productImage,
+  productName,
+  productPrice,
+  deliveryAfter,
+  productLocation,
+  consumerType,
+  sellType,
+  timeOpen,
+  timeClose,
+  vendorDiscount,
+  consumerDiscount,
+  minQuantity,
+  maxQuantity,
+  minCustomer,
+  maxCustomer,
+  minQuantityPerCustomer,
+  maxQuantityPerCustomer,
 }) => {
   return (
     <div className="existing-product-add-item">
@@ -19,7 +56,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
         <div className="existing-product-add-box-row">
           <div className="leftdiv">
             <div className="image-container">
-              <div className="existing_product_checkbox">
+              <div className="existing_product_checkbox z-10">
                 <Checkbox
                   className="border border-solid border-gray-300 data-[state=checked]:!bg-dark-orange"
                   checked={selectedIds?.includes(id)}
@@ -27,61 +64,63 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                 />
                 <img src="/images/hide.png" alt="" />
               </div>
-              <img src="/images/ts-6.png" alt="" />
+              <div className="relative mx-auto my-4 h-36 w-36">
+                <Image
+                  src={
+                    productImage && validator.isURL(productImage)
+                      ? productImage
+                      : PlaceholderImage
+                  }
+                  alt="product-image"
+                  fill
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
+                  className="object-contain"
+                  blurDataURL="/images/product-placeholder.png"
+                  placeholder="blur"
+                />
+              </div>
             </div>
             <div className="text-container">
-              <h3>
-                <a href="">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </a>
-              </h3>
+              <h3>{productName || "-"}</h3>
             </div>
             <div className="form-container">
               <div className="mb-4 grid w-full grid-cols-1 gap-x-3 gap-y-3 md:grid-cols-2">
                 <div className="space-y-2">
                   <div className="text-with-checkagree">
-                    <div className="check-col">
-                      <input
-                        typeof="checkbox"
-                        className="custom-check-s1"
-                        type="checkbox"
-                      />
-                    </div>
                     <label className="text-col" htmlFor="setUpPriceCheck">
                       Stock
                     </label>
                   </div>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>100</span>
+                    <span>-</span>
                   </div>
                 </div>
 
                 <div className="space-y-2">
                   <div className="text-with-checkagree">
-                    <div className="check-col">
-                      <input
-                        typeof="checkbox"
-                        className="custom-check-s1"
-                        type="checkbox"
-                      />
-                    </div>
                     <label className="text-col" htmlFor="setUpPriceCheck">
                       Price
                     </label>
                   </div>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>1000</span>
+                    <span>{productPrice || "-"}</span>
                   </div>
                 </div>
               </div>
               <div className="mb-4 grid w-full grid-cols-1 gap-x-3 gap-y-3 md:grid-cols-2">
                 <div className="flex flex-wrap space-y-2">
                   <Label>Deliver After</Label>
-                  <span>2 days</span>
+                  <span>
+                    {deliveryAfter
+                      ? `${deliveryAfter === 1 ? `${deliveryAfter} day` : `${deliveryAfter} days`}`
+                      : "-"}
+                  </span>
                 </div>
                 <div className="flex flex-wrap space-y-2">
                   <Label>Product Location</Label>
-                  <span>Kolkata</span>
+                  <span>{productLocation || "-"}</span>
                 </div>
               </div>
             </div>
@@ -92,14 +131,14 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                 <div className="flex flex-wrap space-y-2">
                   <label>Time Open</label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>10:00am</span>
+                    <span>{timeOpen || "-"}</span>
                   </div>
                 </div>
 
                 <div className="flex flex-wrap space-y-2">
                   <label>Time Close</label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>7:00pm</span>
+                    <span>{timeClose || "-"}</span>
                   </div>
                 </div>
               </div>
@@ -107,15 +146,15 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
               <div className="mb-4 grid w-full grid-cols-1 gap-x-3 gap-y-3 md:grid-cols-2">
                 <div className="flex flex-wrap space-y-2">
                   <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                    Cosumer Type
+                    Consumer Type
                   </label>
-                  <span>Everyone</span>
+                  <span>{consumerType || "-"}</span>
                 </div>
                 <div className="flex flex-wrap space-y-2">
                   <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                     Sell Type
                   </label>
-                  <span>Normal Sell</span>
+                  <span>{sellType || "-"}</span>
                 </div>
               </div>
 
@@ -125,7 +164,9 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Vendor Discount
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>20%</span>
+                    <span>
+                      {vendorDiscount ? `${vendorDiscount}%` : "-" || "-"}
+                    </span>
                   </div>
                 </div>
                 <div className="flex flex-wrap space-y-2">
@@ -133,7 +174,9 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Consumer Discount
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>20%</span>
+                    <span>
+                      {consumerDiscount ? `${consumerDiscount}%` : "-" || "-"}
+                    </span>
                   </div>
                 </div>
 
@@ -142,7 +185,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Min Quantity
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>2</span>
+                    <span>{minQuantity || "-"}</span>
                   </div>
                 </div>
                 <div className="flex flex-wrap space-y-2">
@@ -150,7 +193,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Max Quantity
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>10</span>
+                    <span>{maxQuantity || "-"}</span>
                   </div>
                 </div>
 
@@ -159,7 +202,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Min Consumer
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>50</span>
+                    <span>{minCustomer || "-"}</span>
                   </div>
                 </div>
                 <div className="flex flex-wrap space-y-2">
@@ -167,7 +210,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Max Consumer
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>1000</span>
+                    <span>{maxCustomer || "-"}</span>
                   </div>
                 </div>
 
@@ -176,7 +219,7 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Min Qty Consumer
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>100</span>
+                    <span>{minQuantityPerCustomer || "-"}</span>
                   </div>
                 </div>
                 <div className="flex flex-wrap space-y-2">
@@ -184,27 +227,13 @@ const ManageProductCard: React.FC<ManageProductCardProps> = ({
                     Max Qty Consumer
                   </label>
                   <div className="theme-inputValue-picker-upDown">
-                    <span>1000</span>
+                    <span>{maxQuantityPerCustomer || "-"}</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        {/* <div className="existing-product-add-box-action">
-      <button type="button" className="custom-btn update">
-        update
-      </button>
-      <button
-        type="button"
-        className="custom-btn theme-primary-btn"
-      >
-        add
-      </button>
-      <button type="button" className="custom-btn edit">
-        edit
-      </button>
-    </div> */}
       </div>
     </div>
   );
