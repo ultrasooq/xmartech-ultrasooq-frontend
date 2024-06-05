f04973aadd0ed16da05616a5e492966ecca55724
Surojit <surojitdas@technoexponent.com>
2024-06-05 21:30:37 +0530

    manage product, rfq design fix

--- app/manage-products/page.tsx
+++ app/manage-products/page.tsx
@@ -258,96 +258,100 @@ const ManageProductsPage = () => {
             className="container m-auto flex px-3"
             onSubmit={form.handleSubmit(onSubmit)}
           >
-            <div className="existing-product-add-lists">
-              {allManagedProductsQuery.isLoading ? (
-                <div className="mx-2 grid w-full grid-cols-3 gap-5">
-                  {Array.from({ length: 3 }).map((_, index) => (
-                    <Skeleton key={index} className="h-96 w-full" />
-                  ))}
-                </div>
-              ) : null}
+            <div className="existing-product-wrap flex">
+              <div className="existing-product-add-lists">
+                {allManagedProductsQuery.isLoading ? (
+                  <div className="mx-2 grid w-full grid-cols-3 gap-5">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                      <Skeleton key={index} className="h-96 w-full" />
+                    ))}
+                  </div>
+                ) : null}
 
-              {!allManagedProductsQuery.data?.data &&
-              !allManagedProductsQuery.isLoading ? (
-                <p className="w-full text-center text-base font-medium">
-                  No data found
-                </p>
-              ) : null}
+                {!allManagedProductsQuery.data?.data &&
+                !allManagedProductsQuery.isLoading ? (
+                  <p className="w-full text-center text-base font-medium">
+                    No data found
+                  </p>
+                ) : null}
 
-              {allManagedProductsQuery.data?.data?.map(
-                (product: {
-                  id: number;
-                  productPrice_product: {
-                    productImages: {
-                      id: number;
-                      image: string | null;
-                      video: string | null;
-                    }[];
-                    productName: string;
-                  };
-                  productPrice: string;
-                  offerPrice: string;
-                  productPrice_productLocation: {
-                    locationName: string;
-                  };
-                  stock: number;
-                  consumerType: string;
-                  sellType: string;
-                  deliveryAfter: number;
-                  timeOpen: number | null;
-                  timeClose: number | null;
-                  vendorDiscount: number | null;
-                  consumerDiscount: number | null;
-                  minQuantity: number | null;
-                  maxQuantity: number | null;
-                  minCustomer: number | null;
-                  maxCustomer: number | null;
-                  minQuantityPerCustomer: number | null;
-                  maxQuantityPerCustomer: number | null;
-                }) => (
-                  <ManageProductCard
-                    selectedIds={selectedProductIds}
-                    onSelectedId={handleProductIds}
-                    key={product?.id}
-                    id={product?.id}
-                    productImage={
-                      product?.productPrice_product?.productImages?.[0]?.image
-                    }
-                    productName={product?.productPrice_product?.productName}
-                    productPrice={product?.productPrice}
-                    offerPrice={product?.offerPrice}
-                    deliveryAfter={product?.deliveryAfter}
-                    productLocation={
-                      product?.productPrice_productLocation?.locationName
-                    }
-                    stock={product?.stock}
-                    consumerType={product?.consumerType}
-                    sellType={product?.sellType}
-                    timeOpen={product?.timeOpen}
-                    timeClose={product?.timeClose}
-                    vendorDiscount={product?.vendorDiscount}
-                    consumerDiscount={product?.consumerDiscount}
-                    minQuantity={product?.minQuantity}
-                    maxQuantity={product?.maxQuantity}
-                    minCustomer={product?.minCustomer}
-                    maxCustomer={product?.maxCustomer}
-                    minQuantityPerCustomer={product?.minQuantityPerCustomer}
-                    maxQuantityPerCustomer={product?.maxQuantityPerCustomer}
+                {allManagedProductsQuery.data?.data?.map(
+                  (product: {
+                    id: number;
+                    productPrice_product: {
+                      productImages: {
+                        id: number;
+                        image: string | null;
+                        video: string | null;
+                      }[];
+                      productName: string;
+                    };
+                    productPrice: string;
+                    offerPrice: string;
+                    productPrice_productLocation: {
+                      locationName: string;
+                    };
+                    stock: number;
+                    consumerType: string;
+                    sellType: string;
+                    deliveryAfter: number;
+                    timeOpen: number | null;
+                    timeClose: number | null;
+                    vendorDiscount: number | null;
+                    consumerDiscount: number | null;
+                    minQuantity: number | null;
+                    maxQuantity: number | null;
+                    minCustomer: number | null;
+                    maxCustomer: number | null;
+                    minQuantityPerCustomer: number | null;
+                    maxQuantityPerCustomer: number | null;
+                  }) => (
+                    <ManageProductCard
+                      selectedIds={selectedProductIds}
+                      onSelectedId={handleProductIds}
+                      key={product?.id}
+                      id={product?.id}
+                      productImage={
+                        product?.productPrice_product?.productImages?.[0]?.image
+                      }
+                      productName={product?.productPrice_product?.productName}
+                      productPrice={product?.productPrice}
+                      offerPrice={product?.offerPrice}
+                      deliveryAfter={product?.deliveryAfter}
+                      productLocation={
+                        product?.productPrice_productLocation?.locationName
+                      }
+                      stock={product?.stock}
+                      consumerType={product?.consumerType}
+                      sellType={product?.sellType}
+                      timeOpen={product?.timeOpen}
+                      timeClose={product?.timeClose}
+                      vendorDiscount={product?.vendorDiscount}
+                      consumerDiscount={product?.consumerDiscount}
+                      minQuantity={product?.minQuantity}
+                      maxQuantity={product?.maxQuantity}
+                      minCustomer={product?.minCustomer}
+                      maxCustomer={product?.maxCustomer}
+                      minQuantityPerCustomer={product?.minQuantityPerCustomer}
+                      maxQuantityPerCustomer={product?.maxQuantityPerCustomer}
+                    />
+                  ),
+                )}
+
+                {allManagedProductsQuery.data?.totalCount > 6 ? (
+                  <Pagination
+                    page={page}
+                    setPage={setPage}
+                    totalCount={allManagedProductsQuery.data?.totalCount}
+                    limit={limit}
                   />
-                ),
-              )}
+                ) : null}
+              </div>
 
-              {allManagedProductsQuery.data?.totalCount > 6 ? (
-                <Pagination
-                  page={page}
-                  setPage={setPage}
-                  totalCount={allManagedProductsQuery.data?.totalCount}
-                  limit={limit}
-                />
-              ) : null}
+              <ManageProductAside
+                isLoading={allManagedProductsQuery.isPending}
+              />
             </div>
-
-            <ManageProductAside isLoading={allManagedProductsQuery.isPending} />
           </form>
         </FormProvider>
       </div>
