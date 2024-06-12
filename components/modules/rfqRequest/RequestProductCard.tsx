db7e4b1792e588da337069b97d7d6ff10549a6d9
aruptechno93 <arup.dey@technoexponent.co.in>
2024-06-12 16:57:22 +0530

    fix: wrap with button element for requestproductcart

--- components/modules/rfqRequest/RequestProductCard.tsx
+++ components/modules/rfqRequest/RequestProductCard.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import Image from "next/image";
-import ProIcon from "@/public/images/pro-2.png";
 import PlaceholderImage from "@/public/images/product-placeholder.png";
+import { cn } from "@/lib/utils";
 
-const RequestProductCard = () => {
+type RequestProductCardProps = {
+  rfqId?: number;
+  onClick?: () => void;
+  isSelected?: boolean;
+};
+
+const RequestProductCard: React.FC<RequestProductCardProps> = ({
+  rfqId,
+  onClick,
+  isSelected,
+}) => {
   return (
-    <div className="flex w-full flex-wrap bg-white px-[10px] py-[15px] shadow-lg">
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        "flex w-full flex-wrap bg-white px-[10px] py-[15px] shadow-lg",
+        isSelected ? "bg-[#F8F8F8]" : "",
+      )}
+    >
       <div className="border-color-[#DBDBDB] h-auto w-16 border border-solid p-1">
         <Image src={PlaceholderImage} alt="placeholder" />
       </div>
-      <div className="font-nromal flex w-[calc(100%-4rem)] items-center justify-start pl-3 text-sm text-[#1D77D1]">
-        Lorem Ipsum is simply dummy text..
+      <div className="font-nromal flex w-[calc(100%-4rem)] items-center justify-start pl-3 pt-2 text-sm text-[#1D77D1]">
+        <span className="text-[#828593]">RFQ ID: </span> RFQ000{rfqId}
       </div>
-    </div>
+    </button>
   );
 };
 
