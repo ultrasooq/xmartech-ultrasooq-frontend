82d6d9e432d7b57839f98ac81448306cb39b202b
aruptechno93 <arup.dey@technoexponent.co.in>
2024-06-20 22:57:06 +0530

    fix: calculate discount at cart,checkout

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -54,6 +54,15 @@ const CartListPage = () => {
     return [];
   }, [cartListByUser.data?.data, cartListByDeviceQuery.data?.data]);
 
+  const calculateDiscountedPrice = (
+    offerPrice: string,
+    consumerDiscount: number,
+  ) => {
+    const price = offerPrice ? Number(offerPrice) : 0;
+    const discount = consumerDiscount || 0;
+    return price - (price * discount) / 100;
+  };
+
   const calculateTotalAmount = () => {
     if (cartListByUser.data?.data?.length) {
       return cartListByUser.data?.data?.reduce(
@@ -62,12 +71,18 @@ const CartListPage = () => {
           curr: {
             productPriceDetails: {
               offerPrice: string;
+              consumerDiscount: number;
             };
             quantity: number;
           },
         ) => {
           return (
-            acc + +(curr.productPriceDetails?.offerPrice ?? 0) * curr.quantity
+            acc +
+            +calculateDiscountedPrice(
+              curr.productPriceDetails?.offerPrice ?? 0,
+              curr?.productPriceDetails?.consumerDiscount,
+            ) *
+              curr.quantity
           );
         },
         0,
@@ -235,6 +250,9 @@ const CartListPage = () => {
                       productImages={
                         item.productPriceDetails?.productPrice_product
                           ?.productImages
+                      }
+                      consumerDiscount={
+                        item.productPriceDetails?.consumerDiscount
                       }
                       onAdd={handleAddToCart}
                       onRemove={handleRemoveItemFromCart}
