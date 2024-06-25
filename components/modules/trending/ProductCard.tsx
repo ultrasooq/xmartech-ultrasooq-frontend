44b1e663cec7c47ec82363da407cd5ccc7e9980d
aruptechno93 <arup.dey@technoexponent.co.in>
2024-06-25 12:44:31 +0530

    update: rename text message

--- components/modules/trending/ProductCard.tsx
+++ components/modules/trending/ProductCard.tsx
@@ -190,9 +190,9 @@ const ProductCard: React.FC<ProductCardProps> = ({
           {item?.askForPrice === "true" ? (
             <button
               type="button"
-              className="inline-block w-full rounded-sm bg-color-yellow px-6 py-1 text-sm font-bold capitalize text-white"
+              className="inline-block w-full rounded-sm bg-color-yellow px-3 py-1 text-sm font-bold text-white"
             >
-              Message
+              Ask vendor for price
             </button>
           ) : (
             <h5 className="py-1 text-[#1D77D1]">
