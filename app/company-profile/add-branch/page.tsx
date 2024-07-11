d0209da260b48e21b5601a8da2206981840e0504
aruptechno93 <arup.dey@technoexponent.co.in>
2024-07-11 14:09:09 +0530

    update: hide chained category component for now

--- app/company-profile/add-branch/page.tsx
+++ app/company-profile/add-branch/page.tsx
@@ -634,7 +634,7 @@ const AddBranchPage = () => {
                     error={form.formState.errors.tagList?.message}
                   />
 
-                  <ChainedCategoryDropdown />
+                  {/* <ChainedCategoryDropdown /> */}
                 </div>
 
                 <div className="mb-3.5 flex w-full border-b-2 border-dashed border-gray-300 pb-4">
